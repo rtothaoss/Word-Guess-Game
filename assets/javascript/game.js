ee44ef079fcbdc18e39595b39712dd64f8ab044a
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2018-12-09 21:01:02 -0600

    cleaned up word tracker

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
+// Global Variables
+// Global Variables
+
 var rappers = ["lil pump", "lil uzi vert", "lil xan", "kodak black", "migos", "post malone"];
 var wins = 0;
 var numberOfGuesses = 12;
 var lettersGuessed = [];
+
 // random rapper generator 
 // random rapper generator 
 var randomRapper = rappers[Math.floor(Math.random() * rappers.length)];
@@ -9,11 +13,16 @@ var randomRapper = rappers[Math.floor(Math.random() * rappers.length)];
 
 
 
-
+// What key is pressed and if statement
+// What key is pressed and if statement
 document.onkeyup = function(event) {
 
 var userGuess = event.key;
 lettersGuessed.push(event.key);
+
+
+// printing text to html
+// printing text to html
 
 var userChoiceText = document.getElementById('userchoice-text');
 var rapperText = document.getElementById('rapper-text');
@@ -43,10 +52,3 @@ if (numberOfGuesses === -1) {
 }
 
 
-
-
-
-
-// var rapperElement = document.getElementById('rapper-text');
-
-// rapperElement.textContent = rappers;
